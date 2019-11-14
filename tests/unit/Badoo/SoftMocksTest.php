361533df52f1869fc4293c2c28158761ff24058d
Rinat Akhmadeev <r.akhmadeev@magiclab.co>
2019-11-14 10:57:46 +0300

    [SERVELAB-1154]: PHP Unit patches

--- tests/unit/Badoo/SoftMocksTest.php
+++ tests/unit/Badoo/SoftMocksTest.php
@@ -9,12 +9,12 @@ namespace Badoo\SoftMock\Tests;
 
 class SoftMocksTest extends \PHPUnit\Framework\TestCase
 {
-    protected function setUp()
+    protected function setUp() : void
     {
         require_once __DIR__ . '/SoftMocksTestClasses.php';
     }
 
-    protected function tearDown()
+    protected function tearDown() : void
     {
         \Badoo\SoftMocks::restoreAll();
         parent::tearDown();
